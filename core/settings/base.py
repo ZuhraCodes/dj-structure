5f104bd8fd0a33c747de00d6cd4696ee74e38bb9
ZuhraCodes <zuhrasayfullayeva31@gmail.com>
2024-12-01 12:07:36 +0500

    allow

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,7 +16,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '45.137.148.70',
+    'api.ipify.org',
+    'www.shadowserver.org'
+]
 
 
 # Application definition
